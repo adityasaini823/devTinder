0595a5740eb5d2e9af3c8ee82a0aa43cc2d1cd84
Aditya <adityasaini823@gmail.com>
2025-04-15 13:07:14 +0530

    added image error

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,26 +2,26 @@ const path = require("path");
 const multer = require("multer");
 // Configure storage for uploaded images
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/"); // Save files in the uploads folder
-    },
-    filename: function (req, file, cb) {
-      cb(null,'image-'+Date.now() + path.extname(file.originalname));
-    }
-  });
-  // File filter to accept only images
-  const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only image files are allowed"), false);
-    }
-  };
-  
-  // Initialize Multer
-  const upload = multer({ 
-    storage,
-    fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB size limit
-  });
-  module.exports=upload
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Save files in the uploads folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, "api/image-" + Date.now() + path.extname(file.originalname));
+  },
+});
+// File filter to accept only images
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+// Initialize Multer
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB size limit
+});
+module.exports = upload;
